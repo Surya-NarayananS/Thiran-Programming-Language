2068afe1a3177ca8433918a6388176475d5a03c1
Surya Narayanan <suryanarayanansg@gmail.com>
2023-04-14 12:19:07 +0530

    Delete Project directory

--- Project/Thiran - Master Code/error.py
+++
@@ -1,7 +0,0 @@
-#######################
-# Code to print errors
-#######################
-
-class Error:
-    def __init__(self, description, char=None):
-        print('\n' + description + ':', char) if char != None else print(description)
